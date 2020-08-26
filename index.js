645662258797a391c22a9aebc30af5bdfea7c6e6
Jacob Shtokolov <jacob.100205@gmail.com>
2020-08-26 22:49:28 +0300

    Moved the texture drawing code away from the UI logic

--- index.js
+++ index.js
@@ -2,9 +2,6 @@
     const canvas = document.createElement('canvas');
     const ctx = canvas.getContext('2d');
 
-    const canvasBuf = document.createElement('canvas');
-    const ctxBuf = canvasBuf.getContext('2d');
-
     const widthInput = document.getElementById('width');
     const heightInput = document.getElementById('height');
 
@@ -74,6 +71,57 @@
     }
 
     /**
+     * Draw entire texture onto specified canvas
+     *
+     * @param {CanvasRenderingContext2D} ctx 
+     * @param {number} width
+     * @param {number} height
+     * @param {number} cols
+     * @param {number} rows
+     * @param {number} margin
+     * @param {Image[]} tiles
+     * @param {CallableFunction} offsetFunc
+     */
+    function drawTexture(ctx, width, height, cols, rows, margin, tiles, offsetFunc) {
+        const tileWidth = (width - (margin * cols)) / cols;
+        const tileHeight = Math.round((height - (margin * rows)) / rows, 2);
+
+        const canvas = ctx.canvas;
+
+        canvas.width = width;
+        canvas.height = height;
+
+        const buffer = document.createElement('canvas');
+        const bufferCtx = buffer.getContext('2d');
+
+        buffer.width = (tileWidth + margin) * cols;
+        buffer.height = tileHeight;
+
+        clearCanvas(ctx);
+
+        ctx.fillStyle = 'grey';
+        ctx.fillRect(0, 0, width, height);
+
+        for (let row = 0; row < rows; row++) {
+            clearCanvas(bufferCtx);
+
+            for (let col = 0; col < cols; col++) {
+                drawRow(bufferCtx, tiles, tileWidth, tileHeight, margin);
+            }
+
+            const offsetX = offsetFunc(row, tileWidth);
+
+            ctx.save();
+            ctx.translate(offsetX, 0);
+            ctx.fillStyle = ctx.createPattern(buffer, 'repeat-x');
+            ctx.fillRect(-1 * offsetX, 0, canvas.width, buffer.height);
+            ctx.restore();
+
+            ctx.translate(0, tileHeight + margin);
+        }
+    }
+
+    /**
      * Randomized offset pattern generator
      *
      * @param {number} row 
@@ -96,54 +144,27 @@
     /**
      * Reset canvas context
      *
-     * @param {HTMLCanvasElement} canvas 
      * @param {CanvasRenderingContext2D} ctx 
      */
-    function clearCanvas(canvas, ctx) {
+    function clearCanvas(ctx) {
         ctx.resetTransform();
-        ctx.clearRect(0, 0, canvas.width, canvas.height);
+        ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
     }
 
     document
         .getElementById('run')
         .addEventListener('click', () => {
             loadImages(tiles.files).then((images) => {
-                const width = parseInt(widthInput.value);
-                const height = parseInt(heightInput.value);
-                const rows = parseInt(rowsInput.value);
-                const cols = parseInt(colsInput.value);
-                const margin = parseInt(marginInput.value);
-                const makeOffset = offsetPatterns[parseInt(offsetInput.value)];
-
-                const tileWidth = (width - (margin * cols)) / cols;
-                const tileHeight = Math.round((height - (margin * rows)) / rows, 2);
-
-                canvas.width = width;
-                canvas.height = height;
-                canvasBuf.width = (tileWidth + margin) * cols;
-                canvasBuf.height = tileHeight;
-
-                clearCanvas(canvas, ctx);
-                ctx.fillStyle = 'grey';
-                ctx.fillRect(0, 0, width, height);
-
-                for (let row = 0; row < rows; row++) {
-                    clearCanvas(canvasBuf, ctxBuf);
-
-                    for (let col = 0; col < cols; col++) {
-                        drawRow(ctxBuf, images, tileWidth, tileHeight, margin);
-                    }
-
-                    const offsetX = makeOffset(row, tileWidth);
-
-                    ctx.save();
-                    ctx.translate(offsetX, 0);
-                    ctx.fillStyle = ctx.createPattern(canvasBuf, 'repeat-x');
-                    ctx.fillRect(-1 * offsetX, 0, canvas.width, canvasBuf.height);
-                    ctx.restore();
-
-                    ctx.translate(0, tileHeight + margin);
-                }
+                drawTexture(
+                    ctx,
+                    parseInt(widthInput.value),
+                    parseInt(heightInput.value),
+                    parseInt(colsInput.value),
+                    parseInt(rowsInput.value),
+                    parseInt(marginInput.value),
+                    images,
+                    offsetPatterns[parseInt(offsetInput.value)]
+                );
 
                 document.body.appendChild(canvas);
             });
